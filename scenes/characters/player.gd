e291fa1ee71a4efebb382eaf6fecf93b04f918e5
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-10-21 02:25:34 -0400

    Add jumping

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 enum States {
 	IDLE,
 	RUNNING,
-	JUMPING,
-	FALLING,
+	ASCENDING,
+	DESCENDING,
 }
 
 const ANIMATION_NODEPATH = "Animation"
@@ -16,25 +16,38 @@ const ACCELERATION_SECONDS = 0.1
 const DECELERATION_SECONDS = 0.1
 
 const TILE_SIZE = 64
-const JUMP_HEIGHT = 3
+const JUMP_HEIGHT = 2.65
+const JUMP_EARLY_RELEASE_DAMPING = 0.5
+const JUMP_BUFFER_TIME = 0.1
 
-const BUFFER_TIME = 0.1
+@onready var gravity = PhysicsServer2D.area_get_param(
+	get_viewport().find_world_2d().space,
+	PhysicsServer2D.AREA_PARAM_GRAVITY
+)
+
+@onready var jump_speed = 2.0 * sqrt(gravity * JUMP_HEIGHT * TILE_SIZE)
 
 const STATES := {
 	States.IDLE: IdleState,
 	States.RUNNING: RunState,
+	States.ASCENDING: AscendingState,
+	States.DESCENDING: DescendingState,
 }
 
 var state_machine := PlayerStateMachine.new(STATES, self)
 
 
+var run_input := 0.0
+
+
 func _ready() -> void:
-	
+	print("jump: " + str(jump_speed))
 	
 	state_machine.change_state_to(States.IDLE)
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	_read_run_input(event)
 	state_machine.unhandled_input(event)
 
 
@@ -46,11 +59,9 @@ func _physics_process(delta: float) -> void:
 	state_machine.physics_process(delta)
 
 
-func run(delta: float, input: int) -> void:
-	var acceleration = (delta / ACCELERATION_SECONDS) * SPEED * input
+func run(delta: float) -> void:
+	var acceleration = (delta / ACCELERATION_SECONDS) * SPEED * run_input
 	velocity.x = clampf(velocity.x + acceleration, -SPEED, SPEED)
-	
-	move_and_slide()
 
 
 func decelerate(delta: float) -> void:
@@ -63,8 +74,23 @@ func decelerate(delta: float) -> void:
 		velocity.x = 0.0
 	else:
 		velocity.x = expected_velocity
-	
-	move_and_slide()
+
+
+func jump() -> void:
+	velocity.y = -jump_speed
+
+
+func dampen_jump() -> void:
+	velocity.y *= JUMP_EARLY_RELEASE_DAMPING
+
+
+func fall(delta: float) -> void:
+	velocity.y += 2.0 * gravity * delta
+
+
+func _read_run_input(event: InputEvent) -> void:
+	if event.is_action("move_left") or event.is_action("move_right"):
+		run_input = Input.get_axis("move_left", "move_right")
 
 
 class PlayerState extends State:
@@ -83,6 +109,12 @@ class PlayerStateMachine extends StateMachine:
 	
 	func _construct_state(state_class) -> State:
 		return state_class.new(player)
+	
+	
+	func change_state_to(state_identifier) -> void:
+		var state_name = States.keys()[state_identifier]
+		print("Changing state to " + state_name)
+		super.change_state_to(state_identifier)
 
 
 class IdleState extends PlayerState:
@@ -93,11 +125,16 @@ class IdleState extends PlayerState:
 		player.get_node(ANIMATION_NODEPATH).play(IDLE_ANIMATION)
 	
 	func unhandled_input(event: InputEvent) -> void:
+		if event.is_action_pressed("jump") and player.is_on_floor():
+			player.jump()
+			state_machine.change_state_to(States.ASCENDING)
+		
 		if event.is_action("move_right") or event.is_action("move_left"):
 			state_machine.change_state_to(States.RUNNING)
 	
 	func physics_process(delta: float) -> void:
 		player.decelerate(delta)
+		player.move_and_slide()
 
 
 class RunState extends PlayerState:
@@ -110,10 +147,58 @@ class RunState extends PlayerState:
 		player.get_node(ANIMATION_NODEPATH).play(RUN_ANIMATION)
 	
 	func unhandled_input(event: InputEvent) -> void:
-		if event.is_action("move_left") or event.is_action("move_right"):
-			input = Input.get_axis("move_left", "move_right")
-			if input == 0.0:
-				state_machine.change_state_to(States.IDLE)
+		if player.run_input == 0.0:
+			state_machine.change_state_to(States.IDLE)
+		
+		if event.is_action_pressed("jump") and player.is_on_floor():
+			player.jump()
+			state_machine.change_state_to(States.ASCENDING)
 	
 	func physics_process(delta: float) -> void:
-		player.run(delta, input)
+		player.run(delta)
+		
+		player.move_and_slide()
+		
+		if not player.is_on_floor():
+			state_machine.change_state_to(States.DESCENDING)
+
+
+class AscendingState extends PlayerState:
+	var jump_dampened = false
+	
+	
+	func unhandled_input(event: InputEvent) -> void:
+		if event.is_action_released("jump") and not jump_dampened:
+			player.dampen_jump()
+			jump_dampened = true
+	
+	func physics_process(delta: float) -> void:
+		if player.run_input != 0.0:
+			player.run(delta)
+		else:
+			player.decelerate(delta)
+		
+		player.fall(delta)
+		
+		player.move_and_slide()
+		
+		if player.velocity.y > 0.0:
+			state_machine.change_state_to(States.DESCENDING)
+
+
+class DescendingState extends PlayerState:
+	func physics_process(delta: float) -> void:
+		if player.run_input != 0.0:
+			player.run(delta)
+		else:
+			player.decelerate(delta)
+		
+		player.fall(delta)
+		
+		player.move_and_slide()
+		
+		if player.is_on_floor():
+			if player.run_input == 0.0:
+				state_machine.change_state_to(States.IDLE)
+			else:
+				state_machine.change_state_to(States.RUNNING)
