beaf73877b9c954d35c4450ac7e2d268e8c20514
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-10-29 19:14:22 -0400

    Add warp ability

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -5,20 +5,26 @@ enum States {
 	RUNNING,
 	ASCENDING,
 	DESCENDING,
+	WARPING,
 }
 
 const ANIMATION_NODEPATH = "Animation"
+const WARP_ANIMATION_NODEPATH = "WarpIndicator/Animation"
 
 # Pixels / sec
 const SPEED = 350
+const FLOAT_SPEED = 25
 
 const ACCELERATION_SECONDS = 0.1
 const DECELERATION_SECONDS = 0.1
+
+const TIME_SCALE_CHANGE_PER_SECOND = 2.0
 
 const TILE_SIZE = 64
 const JUMP_HEIGHT = 2.65
 const JUMP_EARLY_RELEASE_DAMPING = 0.5
 const JUMP_BUFFER_TIME = 0.1
+const WARP_JUMP_SCALE = 0.8
 
 @onready var gravity = PhysicsServer2D.area_get_param(
 	get_viewport().find_world_2d().space,
@@ -32,6 +38,7 @@ const STATES := {
 	States.RUNNING: RunState,
 	States.ASCENDING: AscendingState,
 	States.DESCENDING: DescendingState,
+	States.WARPING: WarpingState,
 }
 
 var state_machine := PlayerStateMachine.new(STATES, self)
@@ -88,6 +95,26 @@ func fall(delta: float) -> void:
 	velocity.y += 2.0 * gravity * delta
 
 
+func start_floating() -> void:
+	var direction = velocity.normalized()
+	velocity = FLOAT_SPEED * direction
+
+
+func point_warp_indicator_at_mouse() -> void:
+	$WarpIndicator.look_at(get_global_mouse_position())
+
+
+func warp() -> void:
+	var to_position: Vector2 = $WarpIndicator/Sprite.global_position
+	var direction := (to_position - position).normalized()
+	
+	var new_velocity = (WARP_JUMP_SCALE * jump_speed) * direction
+	
+	position = to_position
+	velocity = new_velocity
+	print(velocity)
+
+
 func _read_run_input(event: InputEvent) -> void:
 	if event.is_action("move_left") or event.is_action("move_right"):
 		run_input = Input.get_axis("move_left", "move_right")
@@ -129,6 +156,9 @@ class IdleState extends PlayerState:
 			player.jump()
 			state_machine.change_state_to(States.ASCENDING)
 		
+		if event.is_action_pressed("warp"):
+			state_machine.change_state_to(States.WARPING)
+		
 		if event.is_action("move_right") or event.is_action("move_left"):
 			state_machine.change_state_to(States.RUNNING)
 	
@@ -153,6 +183,9 @@ class RunState extends PlayerState:
 		if event.is_action_pressed("jump") and player.is_on_floor():
 			player.jump()
 			state_machine.change_state_to(States.ASCENDING)
+		
+		if event.is_action_pressed("warp"):
+			state_machine.change_state_to(States.WARPING)
 	
 	func physics_process(delta: float) -> void:
 		player.run(delta)
@@ -171,6 +204,9 @@ class AscendingState extends PlayerState:
 		if event.is_action_released("jump") and not jump_dampened:
 			player.dampen_jump()
 			jump_dampened = true
+		
+		if event.is_action_pressed("warp"):
+			state_machine.change_state_to(States.WARPING)
 	
 	func physics_process(delta: float) -> void:
 		if player.run_input != 0.0:
@@ -187,6 +223,10 @@ class AscendingState extends PlayerState:
 
 
 class DescendingState extends PlayerState:
+	func unhandled_input(event: InputEvent) -> void:
+		if event.is_action_pressed("warp"):
+			state_machine.change_state_to(States.WARPING)
+	
 	func physics_process(delta: float) -> void:
 		if player.run_input != 0.0:
 			player.run(delta)
@@ -202,3 +242,32 @@ class DescendingState extends PlayerState:
 				state_machine.change_state_to(States.IDLE)
 			else:
 				state_machine.change_state_to(States.RUNNING)
+
+
+class WarpingState extends PlayerState:
+	func on_entry() -> void:
+		var slow_mo_tween = player.get_tree().create_tween()
+		slow_mo_tween.tween_property(Engine, "time_scale", 0.25, 0.05)
+		player.get_node(WARP_ANIMATION_NODEPATH).play("activate")
+		player.start_floating()
+	
+	func unhandled_input(event: InputEvent) -> void:
+		if event.is_action_released("warp"):
+			player.warp()
+			if player.velocity.y < 0:
+				state_machine.change_state_to(States.ASCENDING)
+			else:
+				state_machine.change_state_to(States.DESCENDING)
+	
+	func process(_delta: float) -> void:
+		player.point_warp_indicator_at_mouse()
+	
+	func physics_process(_delta: float) -> void:
+		#player.adjust_time_scale_towards(delta, 0.5)
+		#player.fall(delta)
+		player.move_and_slide()
+	
+	func on_exit() -> void:
+		var slow_mo_tween = player.get_tree().create_tween()
+		slow_mo_tween.tween_property(Engine, "time_scale", 1.0, 0.05)
+		player.get_node(WARP_ANIMATION_NODEPATH).play("disappear")
